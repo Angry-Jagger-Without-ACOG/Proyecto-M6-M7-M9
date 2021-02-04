f3ec53105246301c7b60f84d5448cdd5cbba00ab
Angry-Jagger-Without-ACOG <dani.fotograf@gmail.com>
2021-02-04 17:43:49 +0100

    Creacion Componentes
    Creacion de los Componentes Login y Registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { LoginComponent } from './Components/login/login.component';
+import { HttpClient } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
