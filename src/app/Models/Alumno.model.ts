f7f138043a2b1cccec707729fa2dad37de8d1f2f
Angry-Jagger-Without-ACOG <dani.fotograf@gmail.com>
2021-02-05 16:30:43 +0100

    Diseño 1 Models
    Primer modelo de las plantilla de los objetos de Alumno y Profesor

---
+++ src/app/Models/Alumno.model.ts
@@ -0,0 +1,13 @@
+export class Alumno {
+
+  constructor(
+    public Mote: string,
+    public Correo : string,
+    public Contraseña : string,
+    public Nombre: string,
+    public Apellido: string,
+    public Curso : string,
+    public Img : string
+){}
+
+}
---
+++ src/app/Models/Profesor.model.ts
@@ -0,0 +1,13 @@
+export class Profesor {
+
+  constructor(
+      public Mote: string,
+      public Correo : string,
+      public Contraseña : string,
+      public Nombre: string,
+      public Apellido: string,
+      public Centro : string,
+      public Img : string
+  ){}
+
+}
