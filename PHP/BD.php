40f6407a1463695f448a7b96d88c9eea2ab5a6ff
Angry-Jagger-Without-ACOG <dani.fotograf@gmail.com>
2021-02-24 19:40:50 +0100

    Arreglo BD.php por cambios del Merge

--- PHP/BD.php
+++ PHP/BD.php
@@ -1,18 +1,12 @@
 <?php
 
-<<<<<<< HEAD
-global $enlace; //variable de tipo global para llamarla en cualquier parte de la aplicacion donde se llame
-function conexion(){
-
- $enlace = mysqli_connect('localhost', 'root', 'usbw', 'test'); //conexion con la base de datos
-=======
 global $enlace;
 
 function conexion(){
 
  $enlace = mysqli_connect('localhost', 'root', 'usbw', 'daw2_gamifikg2');
  //$enlace = mysqli_connect('192.168.3.26', 'DAW2_GamifikG2', 'aGamifikG21', 'daw2_gamifikg2');
->>>>>>> master
+
    mysqli_set_charset($enlace,"utf8");
 
    if(!$enlace){
@@ -24,8 +18,4 @@ function conexion(){
   return $enlace;
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 ?>
